95c7f42010b0d64790bf814ed135c8f76f51fbd3
Justin Man <justin.man@gamma.co.uk>
2018-12-06 16:19:20 +0000

    adds abstract factory code

---
+++ Factory/Java/Factory/src/com/jman/factory/Main.java
@@ -0,0 +1,14 @@
+package com.jman.factory;
+
+/**
+ * The Factory Method Pattern defines an interface for creating
+ * an object, but lets subclasses decide which class to instantiate.
+ * Factory Method lets a class defer instantiation to subclasses
+ *
+* */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Factory/Java/Factory/src/com/jman/factory/product/concreteProduct/CheesePizza.java
@@ -0,0 +1,24 @@
+package com.jman.factory.product.concreteProduct;
+
+import com.jman.factory.creator.PizzaIngredientFactory;
+
+public class CheesePizza extends Pizza {
+
+    PizzaIngredientFactory ingredientFactory;
+
+    public CheesePizza(PizzaIngredientFactory ingredientFactory) {
+        this.ingredientFactory = ingredientFactory;
+    }
+
+    /*
+    * The prepare() method steps through creating a cheese pizza,
+    * each time it needs an ingredient, it asks the factory to produce it
+    * */
+    @Override
+    public void prepare() {
+        System.out.println("Preparing " + name);
+        dough = ingredientFactory.createDough();
+        sauce = ingredientFactory.createSauce();
+        cheese = ingredientFactory.createCheese();
+    }
+}
